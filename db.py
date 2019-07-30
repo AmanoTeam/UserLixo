d0b2872dc0a392d3a4e33ac6b8eebd18dc624c86
OLixao <39718272+olixao@users.noreply.github.com>
2019-07-30 20:57:54 -0300

    updates in db

--- db.py
+++ db.py
@@ -1,6 +1,11 @@
 import json
 
-db = json.load(open('db.json','r'))
+try:
+    db = json.load(open('db.json','r'))
+except FileNotFoundError:
+    with open('db.json','w') as memoria:
+        memoria.write('{"chats": {}, "privates": {}}')
+    db = json.load(open('db.json','r'))
 
 def save(rq):
     mem = open('db.json','w')
