983e1bf91b4d8d5e2516bb40d5dc2c9afd6a346b
OLixao <39718272+olixao@users.noreply.github.com>
2019-07-31 17:04:40 -0300

    upgrade and restart

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
 import backups
 import config
+from db import db, save
+
+if "restart" in db:
+    config.app.start()
+    config.app.edit_message_text(db["restart"]["cid"], db["restart"]["mid"],'Reiniciado')
+    del db["restart"]
+    save(db)
+    config.app.stop()
+
 
 if config.backups_chat:
 	print('ok...')
