1082ee30c2197480673e2f269376fc28ec36274a
OLixao <39718272+olixao@users.noreply.github.com>
2019-07-31 17:29:54 -0300

    s -> ms

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, Filters
-import time
+from datetime import datetime
 
 @Client.on_message(Filters.command("ping", prefix = ['/','!'], separator = '.'))
 def ping(client,message):
-    t1 = time.time()
+    t1 = datetime.now()
     a = message.reply('**Pong!**')
-    t2 = time.time()
-    a.edit(f'**Pong!** `{round((t2 - t1),3)}`s')
+    t2 = datetime.now()
+    a.edit(f'**Pong!** `{(t1 - t2).microseconds / 1000}`ms')
