ec9d6b9481976e647f0378dc4ce23f1377c75138
OLixao <39718272+olixao@users.noreply.github.com>
2019-08-01 13:06:23 -0300

    some additions

---
+++ plugins/save.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, Filters
+
+@Client.on_message(Filters.command("save", prefix = ['/','!']))
+def ping(client,message):
+    if message.reply_to_message:
+        a = message.reply_to_message.forward(message.from_user.id)
+        if message.text.split(' ',1)[1]:
+            a.reply(message.text.split(' ',1)[1])
+    elif message.text.split(' ',1)[1]:
+        client.send_message(message.from_user.id,message.text.split(' ',1)[1])
