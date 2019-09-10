cdc47d9453603db5b4f010207f13d87e12cf3c36
OLixao <39718272+olixao@users.noreply.github.com>
2019-09-10 20:31:01 -0300

    a

--- plugins/fake.py
+++ plugins/fake.py
@@ -18,39 +18,38 @@ def fake(client,message):
                 except:
                     user_id = text
         cha = client.get_chat(user_id)
-        if cha.type != 'private' or cha.type != 'bot':
-            message.reply('only works with profiles')
-        elif cha.id == client.get_me().id:
-            dat = config.personal_data
-            text = 'No fake'
-        else:
-            text = 'new fake'
-            if cha.description:
-                description = cha.description[:69]
+        if cha.type == 'private' or cha.type == 'bot':
+            elif cha.id == client.get_me().id:
+                dat = config.personal_data
+                text = 'No fake'
             else:
-                description = ''
-            if cha.last_name:
-                last_name = cha.last_name
-            else:
-                last_name = ''
-            dat = {
-                    'pic':client.get_profile_photos(user_id, limit=1)[0].file_id,
-                    'description':description,
-                    'last_name':last_name,
-                    'first_name':cha.first_name
-                    }
-        client.send(
-                functions.account.UpdateProfile(
-                    first_name=dat['first_name'], last_name=dat['last_name'],
-                    about=dat['description']
+                text = 'new fake'
+                if cha.description:
+                    description = cha.description[:69]
+                else:
+                    description = ''
+                if cha.last_name:
+                    last_name = cha.last_name
+                else:
+                    last_name = ''
+                dat = {
+                        'pic':client.get_profile_photos(user_id, limit=1)[0].file_id,
+                        'description':description,
+                        'last_name':last_name,
+                        'first_name':cha.first_name
+                        }
+            client.send(
+                    functions.account.UpdateProfile(
+                        first_name=dat['first_name'], last_name=dat['last_name'],
+                        about=dat['description']
+                        )
                     )
-                )
-        try:
-            a = client.download_media(dat['pic'])
-            client.set_profile_photo(a)
-            os.remove(a)
-        except:
-            pass
-        message.reply(text)
-
-        
+            try:
+                a = client.download_media(dat['pic'])
+                client.set_profile_photo(a)
+                os.remove(a)
+            except:
+                pass
+            message.reply(text)
+        else:
+            message.reply('only works with profiles')
