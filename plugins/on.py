4ca0d55ad8b6e55971440389eb48874812349548
Lucas <39718272+olixao@users.noreply.github.com>
2019-11-22 20:53:24 -0300

    Update on.py

--- plugins/on.py
+++ plugins/on.py
@@ -22,12 +22,14 @@ def on(client, message):
         message.reply('This person has disabled his last seen')
     else:
         c = int(time.time()-usr.last_online_date)
-        date = datetime.utcfromtimestamp(c).strftime('{"months":"%-m","days":"%-d","hours":"%-H","minutes":"%-M","seconds":"%-S"}')
+        date = datetime.utcfromtimestamp(c).strftime('{"year":"%y","months":"%-m","days":"%-d","hours":"%-H","minutes":"%-M","seconds":"%-S"}')
         f'{usr.first_name} is off for: \n » **%H** Hours\n » **%M** Minutes\n » **%S** Seconds'
         frase = f'{usr.first_name} is off for: \n'
         date = json.loads(date)
         date["days"] = int(date["days"])-1
         date["months"] = int(date["months"])-1
+        if date["year"][1] != 0:
+            frase += f' » **{date["year"][1]}** year\n'
         if date["months"] != 0:
             frase += f' » **{date["months"]}** months\n'
         if date["days"] != 0:
