425dfcd86ed94fe04f0d67150061770ed99ba0cb
Alisson Lauffer <alissonvitortc@gmail.com>
2019-11-27 18:44:17 -0300

    [restart.py] Remove useless sleep and threading

--- plugins/restart.py
+++ plugins/restart.py
@@ -3,8 +3,7 @@ from config import sudos
 from db import db,save
 import os
 import sys
-import threading
-import time
+
 
 @Client.on_message(Filters.command("restart", prefixes="!"))
 def restart(client, message):
@@ -12,6 +11,4 @@ def restart(client, message):
         sent = message.reply('Reiniciando...')
         db["restart"] = {'cid':message.chat.id, 'mid':sent.message_id}
         save(db)
-        time.sleep(1)
         os.execl(sys.executable, sys.executable, *sys.argv)
-        del threading.Thread
