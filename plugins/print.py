8c5b8223e5315f96a6433e1a5851dfc1508992e0
Lucas <39718272+olixao@users.noreply.github.com>
2019-12-16 13:54:32 -0300

    [print.py] use chomeprinter

--- plugins/print.py
+++ plugins/print.py
@@ -1,34 +1,19 @@
 from pyrogram import Client, Filters
-import requests
 import time
 import re
 import os
+import chomeprinter
+
+a = chomeprinter.Client()
 
 @Client.on_message(Filters.command("print", prefixes = ['!','/']))
 def print(client, message):
     url = message.text.split(' ',1)[1]
-    if 'fullpage ' in url:
-        ctime = time.time()
-        try:
-            if not re.match(r'^[a-z]+://', url):
-                url = 'http://'+url
-            r = requests.post("https://api.thumbnail.ws/api/ab45a17344aa033247137cf2d457fc39ee4e7e16a464/thumbnail/get", params=dict(url=url.replace('fullpage ',''),width=1280, refresh='true', fullpage='true'))
-            with open(f'{ctime}.png', 'wb') as f:
-                f.write(r.content)
-            client.send_photo(message.chat.id,f"{ctime}.png")
-        except Exception as e:
-            message.reply(f'There was an error sending print, please try again later. \nDescription of the error:{e}')
-        os.remove(f'{ctime}.png')
-        ctime = time.time()
+    ctime = time.time()
+    if re.match(r'^[a-z]+://', url):
+        url = url
     else:
-        ctime = time.time()
-        try:
-            if not re.match(r'^[a-z]+://', url):
-                url = 'http://'+url
-            r = requests.post("http://amn-api.herokuapp.com/print", params=dict(q=url))
-            with open(f'{ctime}.png', 'wb') as f:
-                f.write(r.content)
-            client.send_photo(message.chat.id,f"{ctime}.png")
-        except Exception as e:
-            message.reply(f'There was an error sending print, please try again later. \nDescription of the error:{e}')
-        os.remove(f'{ctime}.png')
+        url = 'http://'+url
+    a.make_screenshot(url,f'{ctime}.png')
+    client.send_photo(message.chat.id,f"{ctime}.png")
+    os.remove(f'{ctime}.png')
