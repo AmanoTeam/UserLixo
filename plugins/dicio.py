2beec65f79eff7fca3aa92931e8391ff1d5dc2eb
Lucas <39718272+olixao@users.noreply.github.com>
2019-12-16 15:06:05 -0300

    [dicio.py] fix /dicio

--- plugins/dicio.py
+++ plugins/dicio.py
@@ -4,5 +4,6 @@ import dicioinformal
 @Client.on_message(Filters.command("dicio", prefixes="/"))
 def dicio(client, message):
     txt = message.text.split(' ',1)[1]
-    a = dicioinformal.definicao(txt)
-    message.reply(a)
+    a = dicioinformal.definicao(txt)[0]
+    frase = f'{a["title"]}:\n{a["tit"]}\n\n__{a["desc"]}__'
+    message.reply(frase)
