9bf02a88c964a604f571f05d738416553b736e68
Lucas Gabriel <ribru29@gmail.com>
2020-01-30 09:25:52 -0300

    add tagall and GenerateStringSession.py

---
+++ GenerateStringSession.py
@@ -0,0 +1,24 @@
+print('Confifurando...')
+con = open('config.py.exemple','r').read()
+a = ['api_id','api_hash']
+
+for i in a:
+    exec(f"{i} = input('{i}: ')")
+    con = con.replace(f"{i} = ''",f"{i} = '{eval(i)}'")
+
+from config import app
+
+print('login')
+app.start()
+b = app.get_chat(app.get_me().id)
+app.stop()
+con = con.replace('кк',b.first_name)
+if b.last_name:
+    con = con.replace('εαε мεη',b.last_name)
+else:
+    con = con.replace('εαε мεη','')
+if b.description:
+    con = con.replace('userbot da @AmanoTeam',b.description)
+else:
+    con = con.replace('userbot da @AmanoTeam','')
+open('config.py','w').write(con)
---
+++ plugins/tagall.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, Filters
+
+@Client.on_message(Filters.command("tagall", prefixes=".") & Filters.me)
+def tagall(client, message):
+    a = message.text.split(' ',1)[1]
+    for x in client.iter_chat_members(message.chat.id):
+        if x.user.status == 'online':
+            a += f"\n[{x.user.first_name}](tg://user?id={x.user.id})"
+    message.edit(a)
+    
+@Client.on_message(Filters.command("admin", prefixes=".") & Filters.me)
+def admin(client, message):
+    a = message.text.split(' ',1)[1]
+    for x in client.iter_chat_members(message.chat.id, filter='administrators'):
+        if x.user.status == 'online':
+            a += f"\n[{x.user.first_name}](tg://user?id={x.user.id})"
+    message.edit(a)
