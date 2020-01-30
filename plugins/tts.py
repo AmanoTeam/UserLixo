b6aefb148302184d208b13f2ca2faff5b7ea87c8
Lucas Gabriel <ribru29@gmail.com>
2020-01-30 10:34:36 -0300

    [tts.py] ok

--- plugins/tts.py
+++ plugins/tts.py
@@ -1,21 +1,32 @@
 import os
 import time
 
-from gtts import gTTS
+from gtts import gTTS, lang
 from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command("tts", prefixes=".") & Filters.me)
 def oi(client, message):
-    if message.reply_to_message:
-        txt = message.reply_to_message.text
-        lang = message.text[5:]
+    langs = False
+    txt = False
+    a = message.text[5:]
+    if '|' in a:
+        langs, txt = a.split('|', 1)
     else:
-        a = message.text[5:]
-        lang, txt = a.split(' ', 1)
-    tts = gTTS(txt, lang=lang)
+        if a in lang.tts_langs():
+            langs = a
+        else:
+            txt = a
+    if not langs:
+        langs = 'pt-BR'
+    if not txt and message.reply_to_message:
+        txt = message.reply_to_message.text
+    tts = gTTS(txt, lang=langs)
     ctime = time.time()
     tts.save(f'{ctime}.mp3')
     message.delete()
-    client.send_voice(message.chat.id, f'{ctime}.mp3')
+    if message.reply_to_message:
+        client.send_voice(message.chat.id, f'{ctime}.mp3', reply_to_message_id=message.reply_to_message.message_id)
+    else:
+        client.send_voice(message.chat.id, f'{ctime}.mp3')
     os.remove(f'{ctime}.mp3')
