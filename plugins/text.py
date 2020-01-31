ba51e3720886c259067ec17a7c4331d259810df3
Alisson Lauffer <alissonvitortc@gmail.com>
2020-01-31 03:26:42 -0300

    [text.py] Better typewritter.

--- plugins/text.py
+++ plugins/text.py
@@ -4,14 +4,13 @@ from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command("text", prefixes=".") & Filters.me)
-def ping(client, message):
+def text(client, message):
     ch = ''
-    text = message.text.split(' ', 1)[1]
+    txt = message.text.split(' ', 1)[1]
     ms = message.edit('`|`')
-    for i in text:
-        ch = ch + i
-        ms.edit(f'`{ch}`')
+    for i in txt:
+        ch += i
+        ms = ms.edit(f'`{ch}|`')
         sleep(0.3)
-        ms.edit(f'`{ch}|`')
+        ms = ms.edit(f'`{ch.strip()}`')
         sleep(0.3)
-    ms.edit(f'`{text}`')
