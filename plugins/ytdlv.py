ecf2b8c6d91759ea50d5e08584875ba47eafc673
Lucas Gabriel <ribru29@gmail.com>
2020-02-02 21:20:05 -0300

    Update ytdlv.py

--- plugins/ytdlv.py
+++ plugins/ytdlv.py
@@ -13,7 +13,14 @@ last_edit = 0
 @Client.on_message(Filters.command("ytdlv", prefixes=".") & Filters.me)
 def ytdlv(client, message):
     url = message.text[7:]
-    ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s-%(id)s.%(ext)s', 'noplaylist': True})
+    if '-m4a' in url:
+        url = url.replace(' -m4a','')
+        ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s-%(id)s.%(ext)s', 'format': '140', 'noplaylist': True})
+        vid = False
+    else:
+        url = url.replace(' -mp4','')
+        ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s-%(id)s.%(ext)s', 'noplaylist': True})
+        vid = True
     if 'youtu.be' not in url and 'youtube.com' not in url:
         yt = ydl.extract_info('ytsearch:' + url, download=False)['entries'][0]
         url = 'https://www.youtube.com/watch?v=' + yt['id']
@@ -31,13 +38,23 @@ def ytdlv(client, message):
     # Workaround for when youtube-dl changes file extension without telling us.
     filename = ydl.prepare_filename(yt).rsplit(".", 1)[0]
     filename = glob(f"{filename}.*")[0]
-
-    client.send_video(message.chat.id, filename, caption=yt["title"], duration=yt['duration'],
-                      thumb=f'{ctime}.png', progress=progress, progress_args=(client, message, a))
+    
+    if vid:
+        client.send_video(message.chat.id, filename, caption=yt["title"], duration=yt['duration'],
+                          thumb=f'{ctime}.png', progress=progress, progress_args=(client, message, a))
+    else:
+        if ' - ' in yt["title"]:
+            performer, title = yt["title"].rsplit(' - ', 1)
+        else:
+            performer = yt.get('creator') or yt.get('uploader')
+            title = yt["title"]
+        client.send_audio(message.chat.id, filename, title=title, performer=performer, duration=yt['duration'],
+                          thumb=f'{ctime}.png', progress=progress, progress_args=(client, message, a))
+        
     message.delete()
     os.remove(filename)
     os.remove(f'{ctime}.png')
-
+    
 
 def progress(current, total, c, m, a):
     global last_edit
