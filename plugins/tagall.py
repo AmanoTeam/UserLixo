033d7bc565e3816866c95c6786d018bde6e972e3
Lucas Gabriel <ribru29@gmail.com>
2020-02-05 18:05:01 -0300

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -1,21 +1,29 @@
 from pyrogram import Client, Filters
 
 
-@Client.on_message(Filters.command("tagall", prefixes=".") & Filters.me)
+@Client.on_message(Filters.command(["tagall","tagalln"], prefixes=".") & Filters.me)
 def tagall(client, message):
     message.delete()
-    a = message.text.split(' ', 1)[1] or ''
+    n = True
+    b = message.text.split(' ', 1)
+    a = b[1]
+    if b[0] == '.tagalln':
+        n = False
     for x in client.iter_chat_members(message.chat.id):
-        if x.user.status == 'online':
+        if n and x.user.status == 'online':
             a += f"[‌](tg://user?id={x.user.id})"
     client.send_message(message.chat.id, a,reply_to_message_id=message.message_id)
 
 
-@Client.on_message(Filters.command("admin", prefixes=".") & Filters.me)
+@Client.on_message(Filters.command(["admin","adminn"], prefixes=".") & Filters.me)
 def admin(client, message):
     message.delete()
-    a = message.text.split(' ', 1)[1] or ''
+    n = True
+    b = message.text.split(' ', 1)
+    a = b[1]
+    if b[0] == '.adminn':
+        n = False
     for x in client.iter_chat_members(message.chat.id, filter='administrators'):
-        if x.user.status == 'online':
+        if n and x.user.status == 'online':
             a += f"[­](tg://user?id={x.user.id})"
     client.send_message(message.chat.id, a)
