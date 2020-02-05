5cc67d29d9ec9c41decb48aa41e72aa2ce2ee800
Lucas Gabriel <ribru29@gmail.com>
2020-02-05 18:07:46 -0300

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -1,29 +1,38 @@
 from pyrogram import Client, Filters
 
 
-@Client.on_message(Filters.command(["tagall","tagalln"], prefixes=".") & Filters.me)
+@Client.on_message(Filters.command("tagall", prefixes=".") & Filters.me)
 def tagall(client, message):
     message.delete()
-    n = True
-    b = message.text.split(' ', 1)
-    a = b[1]
-    if b[0] == '.tagalln':
-        n = False
+    a = message.text.split(' ', 1)[1] or ''
     for x in client.iter_chat_members(message.chat.id):
-        if n and x.user.status == 'online':
+        if x.user.status == 'online':
             a += f"[‌](tg://user?id={x.user.id})"
     client.send_message(message.chat.id, a,reply_to_message_id=message.message_id)
 
 
-@Client.on_message(Filters.command(["admin","adminn"], prefixes=".") & Filters.me)
+@Client.on_message(Filters.command("admin", prefixes=".") & Filters.me)
 def admin(client, message):
     message.delete()
-    n = True
-    b = message.text.split(' ', 1)
-    a = b[1]
-    if b[0] == '.adminn':
-        n = False
+    a = message.text.split(' ', 1)[1] or ''
     for x in client.iter_chat_members(message.chat.id, filter='administrators'):
-        if n and x.user.status == 'online':
+        if x.user.status == 'online':
             a += f"[­](tg://user?id={x.user.id})"
+    client.send_message(message.chat.id, a)
+    
+@Client.on_message(Filters.command("tagalln", prefixes=".") & Filters.me)
+def tagalln(client, message):
+    message.delete()
+    a = message.text.split(' ', 1)[1] or ''
+    for x in client.iter_chat_members(message.chat.id):
+        a += f"[‌](tg://user?id={x.user.id})"
+    client.send_message(message.chat.id, a,reply_to_message_id=message.message_id)
+
+
+@Client.on_message(Filters.command("adminn", prefixes=".") & Filters.me)
+def adminn(client, message):
+    message.delete()
+    a = message.text.split(' ', 1)[1] or ''
+    for x in client.iter_chat_members(message.chat.id, filter='administrators'):
+        a += f"[­](tg://user?id={x.user.id})"
     client.send_message(message.chat.id, a)
