2d91705e02244e5984920dbf6f3fd0414d53dd70
Lucas Gabriel <ribru29@gmail.com>
2020-02-10 20:43:58 -0300

    [upgrade.py] fix

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -1,23 +1,28 @@
 import os
-import subprocess
 import sys
-import threading
-import time
+import asyncio
 
 from pyrogram import Client, Filters
 
-from config import git_repo
 from db import db, save
 
 
 @Client.on_message(Filters.command("upgrade", prefixes=".") & Filters.me)
-async def uprade(client, message):
-    a = await client.send_message('me', 'Atualizando...')
-    out = subprocess.getstatusoutput('git pull {}'.format(' '.join(git_repo)))[1]
-    a.edit(f'Resultado da atualização:\n{out}')
-    sent = await message.edit('Reiniciando...')
-    db["restart"] = {'cid': message.chat.id, 'mid': sent.message_id}
-    save(db)
-    time.sleep(1)
-    os.execl(sys.executable, sys.executable, *sys.argv)
-    del threading.Thread
+async def upgrade(client, message):
+    await message.edit("Upgrading sources...")
+    proc = await asyncio.create_subprocess_shell(f"git pull --no-edit",
+                                                 stdout=asyncio.subprocess.PIPE,
+                                                 stderr=asyncio.subprocess.STDOUT)
+    stdout = (await proc.communicate())[0]
+    if proc.returncode == 0:
+        if "Already up to date." in stdout.decode():
+            await message.edit("There's nothing to upgrade.")
+        else:
+            await message.edit(("reiniciando..."))
+            db["restart"] = {'cid': message.chat.id, 'mid': message.message_id}
+            save(db)
+            os.execl(sys.executable, sys.executable, *sys.argv)
+    else:
+        await message.edit(f"Upgrade failed (process exited with {proc.returncode}):\n{stdout.decode()}")
+        proc = await asyncio.create_subprocess_shell("git pull --no-edit")
+        stdout = await proc.communicate()
