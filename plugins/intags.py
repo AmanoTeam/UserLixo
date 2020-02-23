14545b749c928da102b91333301db55c4a32262f
Cezar H <29507335+usernein@users.noreply.github.com>
2020-02-23 15:11:50 -0300

    Adding intag <#note>

--- plugins/intags.py
+++ plugins/intags.py
@@ -6,6 +6,7 @@ import traceback
 import subprocess
 from contextlib import redirect_stdout
 from config import cmds
+from db import db
 
 @Client.on_message(Filters.regex(r'.*<py>.+</py>', re.S) & Filters.me)
 async def pytag(client, message):
@@ -33,7 +34,25 @@ async def shtag(client, message):
         message.text = message.text.replace(match[0], html.escape(out))
     await message.edit(message.text)
 
+@Client.on_message(Filters.regex(r'.*<#.+?>') & Filters.me)
+async def sharptag(client, message):
+	changed = False
+    for match in re.finditer(r'<#(.+?)>', message.text):
+        note_key = match[1]
+        exists = note_key in db['notes']
+        
+        if exists:
+            changed = True
+            note_obj = db['notes'][note_key]
+            if note_obj['type'] == 'text':
+                text = note_obj['value']
+                message.text = message.text.replace(match[0], html.escape(text))
+    if changed:
+        await message.edit(message.text)
+        
+
 cmds.update({
     '<py>':'Run the python code inside the tag and replace it with the result',
-    '<sh>':'Run the shell command inside the tag and replace it with the result'
+    '<sh>':'Run the shell command inside the tag and replace it with the result',
+    '<#%note>': "Search for a note named %note (replace with the name of the wanted note) and replace the tag with it",
 })
