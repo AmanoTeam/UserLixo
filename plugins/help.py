37fa9ad6d6bf291b0a43dda1d2a0541d74950ccc
Cezar H <29507335+usernein@users.noreply.github.com>
2020-02-23 00:57:29 -0300

    Rename about.py to help.py

--- plugins/help.py
+++ plugins/help.py
@@ -6,8 +6,13 @@ from pyrogram import Client, Filters
 async def chelp(client, message):
     if message.text[6:]:
         a = message.text[6:]
-        if a:
-            await message.edit(cmds[a])
+        if a in cmds:
+            await message.edit(f'{a}: {cmds[a]}')
+        else:
+            await message.edit(f'Command \'{a}\' not found.')
+            
     else:
         a = ['{}: {}'.format(i, cmds[i]) for i in cmds]
         await message.edit('\n'.join(a))
+
+cmds.update({'.help':'List all the commands'})
