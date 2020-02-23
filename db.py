d6ea063203b22f2ca815a7ba9617b011f3ff159b
Cezar H <29507335+usernein@users.noreply.github.com>
2020-02-23 09:30:55 -0300

    Fix indentation error

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import json
 
 try:
-	with open('db.json', 'r') as fp:
+    with open('db.json', 'r') as fp:
         db = json.load(fp)
 except FileNotFoundError:
-	db = {"chats": {}, "privates": {}, "personal_data":{}, "notes":{}}
+    db = {"chats": {}, "privates": {}, "personal_data":{}, "notes":{}}
     with open('db.json', 'w') as fp:
         json.dump(db, fp)
 
