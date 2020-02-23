400aa7413af2ccba463b6318ce3467b13bfdeae4
Cezar H <29507335+usernein@users.noreply.github.com>
2020-02-23 13:40:02 -0300

    Changing db.py to automatically create needed keys

--- db.py
+++ db.py
@@ -1,10 +1,15 @@
 import json
+default_db = {"chats": {}, "privates": {}, "personal_data":{}, "notes":{}}
 
 try:
     with open('db.json', 'r') as fp:
         db = json.load(fp)
+        
+    for key,val in default_db.items():
+        if key not in db:
+            db[key] = val
 except FileNotFoundError:
-    db = {"chats": {}, "privates": {}, "personal_data":{}, "notes":{}}
+    db = default_db
     with open('db.json', 'w') as fp:
         json.dump(db, fp)
 
