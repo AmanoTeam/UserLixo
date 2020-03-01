f70c53535cfb4808f1f9ef403da8b54030187250
Cezar H <29507335+usernein@users.noreply.github.com>
2020-02-29 21:58:59 -0300

    Fix sed.py

--- plugins/sed.py
+++ plugins/sed.py
@@ -9,7 +9,7 @@ async def onsed(client, message):
         return
     match = message.matches[0]
     flags = 0
-    if 'flags' in match.groupdict():
+    if match['flags']:
         for flag in match['flags']:
             flag = switch_case(flag.lower(), {
                 'i': re.I,
