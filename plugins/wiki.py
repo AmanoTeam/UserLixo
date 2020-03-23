12175de689b985695587f0550d827e2a242321dd
Alisson Lauffer <alissonvitortc@gmail.com>
2020-03-23 19:55:48 -0300

    [wiki.py] Inform if no result is found

--- plugins/wiki.py
+++ plugins/wiki.py
@@ -9,8 +9,11 @@ import wikipedia
 async def dwiki(client, message):
     txt = message.text[7:]
     a = desciclopedia.search(txt)
-    a = desciclopedia.page(a[0])
-    await message.edit(f'[{a.title}]({a.url}):\n\n{a.content[:910]+"..."}')
+    if a:
+        a = desciclopedia.page(a[0])
+        await message.edit(f'[{a.title}]({a.url}):\n\n{a.content[:910]+"..."}')
+    else:
+        await message.edit(f"No results found for \"`{txt}`\"")
 
 
 @Client.on_message(Filters.command("wiki", prefixes='.') & Filters.me)
@@ -18,8 +21,11 @@ async def wiki(client, message):
     txt = message.text[6:]
     a = wikipedia.search(txt)
     wikipedia.set_lang('pt')
-    a = wikipedia.page(a[0])
-    await message.edit(f'[{a.title}]({a.url}):\n\n{a.content[:910]+"..."}')
+    if a:
+        a = wikipedia.page(a[0])
+        await message.edit(f'[{a.title}]({a.url}):\n\n{a.content[:910]+"..."}')
+    else:
+        await message.edit(f"No results found for \"`{txt}`\"")
 
 cmds.update({'.wiki':'Search meaning of a word on wikipedia',
              '.dwiki':'Search meaning of a word on desciclopedia'})
