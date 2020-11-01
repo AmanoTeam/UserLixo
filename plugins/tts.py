f95e9aeb5e469e53b27a366cb512ebe0605bd57d
Alisson Lauffer <alissonvitortc@gmail.com>
2020-11-01 02:14:52 -0300

    [tts.py] Plugin refactor
    - Cache available langs during start
    - Save gtts file to BytesIO
    - Change some variable names with more descriptive names

--- plugins/tts.py
+++ plugins/tts.py
@@ -1,44 +1,56 @@
 import os
 import time
+import logging
 
 from config import cmds
 
+from io import BytesIO
 from gtts import gTTS, lang
 from pyrogram import Client, filters
 
 
+try:
+    langs = lang.tts_langs()
+except RuntimeError:
+    logging.warning(f"[{__name__}] Unable to cache the list of tts languages. Will be tried again in the first time you use the command.")
+    langs = None
+
+
 @Client.on_message(filters.command("tts", prefixes=".") & filters.me)
 async def tts(client, message):
-    langs = False
-    txt = False
-    a = message.text[5:]
-    if '|' in a:
-        langs, txt = a.split('|', 1)
-    else:
-        if a in lang.tts_langs():
-            langs = a
-        else:
-            txt = a
+    global langs
     if not langs:
-        langs = 'pt-BR'
-    if not txt and message.reply_to_message:
+        langs = lang.tts_langs()
+    text: str = None
+    mtext = message.text[5:]
+    if '|' in mtext:
+        tlang, text = mtext.split('|', 1)
+    else:
+        if mtext in langs:
+            tlang = mtext
+        else:
+            text = mtext
+            tlang = 'pt-BR'
+    if not text and message.reply_to_message:
         if message.reply_to_message.text:
-            txt = message.reply_to_message.text
+            text = message.reply_to_message.text
         elif message.reply_to_message.document:
             path = await message.reply_to_message.download(f'{message.message_id}_{message.reply_to_message.message_id}_tts')
             with open(path) as fp:
-                txt = fp.read()
-                os.remove(fp.name)
+                text = fp.read()
+            os.remove(path)
         else:
-            return await message.edit('Nothing to use')
-    gtts = gTTS(txt, lang=langs)
-    ctime = time.time()
-    gtts.save(f'{ctime}.mp3')
-    await message.delete()
-    if message.reply_to_message:
-        await client.send_voice(message.chat.id, f'{ctime}.mp3', reply_to_message_id=message.reply_to_message.message_id)
+            return await message.edit_text('Nothing to use')
+    gtts = gTTS(text, lang=tlang)
+    bio = BytesIO()
+    bio.name = "audio.mp3"
+    try:
+        gtts.write_to_fp(bio)
+    except ValueError as e:
+        await message.edit_text(f"Error: {e}", disable_web_page_preview=True)
     else:
-        await client.send_voice(message.chat.id, f'{ctime}.mp3')
-    os.remove(f'{ctime}.mp3')
+        await message.delete()
+        reply_to = message.reply_to_message.message_id if message.reply_to_message else None
+        await client.send_voice(message.chat.id, bio, reply_to_message_id=reply_to)
 
 cmds.update({'.tts':'Convert text to speech with Google APIs'})
