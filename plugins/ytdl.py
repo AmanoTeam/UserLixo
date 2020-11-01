58c4b0b712cb22e4ff7c72a8f2a9b986f3c941f1
Alisson Lauffer <alissonvitortc@gmail.com>
2020-11-01 00:47:01 -0300

    Revert "Delete ytdl.py"
    This reverts commit e8550870542c737c980ebdb13ae3411391dd365b.

---
+++ plugins/ytdl.py
@@ -0,0 +1,79 @@
+import os
+import time
+
+import aiohttp
+import youtube_dl
+from config import cmds
+from utils import aiowrap
+from pyrogram import Client, filters
+from pyrogram.errors import MessageNotModified
+
+last_edit = 0
+
+
+@aiowrap
+def extract_info(instance, url, download=True):
+    return instance.extract_info(url, download)
+
+
+@Client.on_message(filters.command("ytdl", prefixes=".") & filters.me)
+async def ytdl(client, message):
+    url = message.text.split(' ',1)[1]
+    if '-m4a' in url:
+        url = url.replace(' -m4a','')
+        ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s-%(id)s.%(ext)s', 'format': '140', 'noplaylist': True})
+        vid = False
+    else:
+        url = url.replace(' -mp4','')
+        ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s-%(id)s.%(ext)s', 'format': 'mp4', 'noplaylist': True})
+        vid = True
+    if 'youtu.be' not in url and 'youtube.com' not in url:
+        yt = await extract_info(ydl, 'ytsearch:' + url, download=False)
+        yt = yt['entries'][0]
+        url = 'https://www.youtube.com/watch?v=' + yt['id']
+    else:
+        yt = await extract_info(ydl, url, download=False)
+        url = 'https://www.youtube.com/watch?v=' + yt['id']
+    await message.edit(f'Downloading `{yt["title"]}`')
+    yt = await extract_info(ydl, url, download=True)
+    a = f'Sending `{yt["title"]}`'
+    await message.edit(a)
+    ctime = time.time()
+    async with aiohttp.ClientSession() as session:
+        r = await session.get(yt['thumbnail'])
+        with open(f'{ctime}.png', 'wb') as f:
+            f.write(await r.read())
+    # Workaround for when youtube-dl changes file extension without telling us.
+    filename = ydl.prepare_filename(yt)
+
+
+    if vid:
+        await client.send_video(message.chat.id, filename, width=int(1920), height=int(1080), caption=yt["title"], duration=yt['duration'],
+                          thumb=f'{ctime}.png', progress=progress, progress_args=(client, message, a))
+    else:
+        if ' - ' in yt["title"]:
+            performer, title = yt["title"].rsplit(' - ', 1)
+        else:
+            performer = yt.get('creator') or yt.get('uploader')
+            title = yt["title"]
+        await client.send_audio(message.chat.id, filename,  title=title, performer=performer, duration=yt['duration'],
+                          thumb=f'{ctime}.png', progress=progress, progress_args=(client, message, a))
+
+    await message.delete()
+    os.remove(filename)
+    os.remove(f'{ctime}.png')
+
+
+async def progress(current, total, c, m, a):
+    global last_edit
+    percent = current * 100 / total
+    if last_edit + 1 < int(time.time()) or current == total:
+        await c.send_chat_action(m.chat.id, 'UPLOAD_VIDEO')
+        try:
+            await m.edit(a + '\n' + "{:.1f}%".format(percent))
+        except MessageNotModified:
+            pass
+        finally:
+            last_edit = int(time.time())
+
+cmds.update({'.ytdl':'Download a youtube video'})
