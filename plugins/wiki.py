e09077f8f3ee5510b2eefe866b29b2669a1b4b84
RianFC <ribru29@gmail.com>
2021-07-27 14:01:45 -0300

    Use telegra.ph in .wiki and .dwiki

--- plugins/wiki.py
+++ plugins/wiki.py
@@ -1,9 +1,13 @@
 from pyrogram import Client, filters
+from telegraph import Telegraph
 
 from config import cmds
 
 import desciclopedia
 import wikipedia
+
+telegraph = Telegraph()
+telegraph.create_account(short_name="UserLixo", author_name="amn")
 
 @Client.on_message(filters.command("dwiki", prefixes='.') & filters.me)
 async def dwiki(client, message):
@@ -11,7 +15,13 @@ async def dwiki(client, message):
     a = desciclopedia.search(txt)
     if a:
         a = desciclopedia.page(a[0])
-        await message.edit(f'[{a.title}]({a.url}):\n\n{a.content[:910]+"..."}')
+        response = telegraph.create_page(
+            a.title,
+            html_content=a.content,
+            author_name="Desciclopedia",
+            author_url=a.url
+        )
+        await message.edit(response["url"])
     else:
         await message.edit(f"No results found for \"`{txt}`\"")
 
@@ -23,7 +33,13 @@ async def wiki(client, message):
     wikipedia.set_lang('pt')
     if a:
         a = wikipedia.page(a[0])
-        await message.edit(f'[{a.title}]({a.url}):\n\n{a.content[:910]+"..."}')
+        response = telegraph.create_page(
+            a.title,
+            html_content=a.content,
+            author_name="Wikipedia",
+            author_url=a.url
+        )
+        await message.edit(response["url"])
     else:
         await message.edit(f"No results found for \"`{txt}`\"")
 
