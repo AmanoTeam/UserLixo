1317e4619d7a0929aa5cb7592962e75a0840caaa
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-27 12:21:23 -0300

    feat(userbot): add cmd controller and handler

--- userlixo/userbot/controllers/message/__init__.py
+++ userlixo/userbot/controllers/message/__init__.py
@@ -1,10 +1,19 @@
 from userlixo.decorators import Controller
 from .about_controller import AboutController
+from .cmd_controller import CmdController
 from .info_controller import InfoController
 from .ping_controller import PingController
 from .plugin_controller import PluginController
 
 
-@Controller(imports=[AboutController, PingController, InfoController, PluginController])
+@Controller(
+    imports=[
+        AboutController,
+        PingController,
+        InfoController,
+        PluginController,
+        CmdController,
+    ]
+)
 class MessageController:
     pass
