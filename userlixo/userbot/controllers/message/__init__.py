ef556419e841d17b9a8b72ce3b709c719bb99b47
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-27 11:46:48 -0300

    feat(userbot): add info controller and handler

--- userlixo/userbot/controllers/message/__init__.py
+++ userlixo/userbot/controllers/message/__init__.py
@@ -1,7 +1,9 @@
 from userlixo.decorators import Controller
 from .about_controller import AboutController
+from .info_controller import InfoController
+from .ping_controller import PingController
 
 
-@Controller(imports=[AboutController])
+@Controller(imports=[AboutController, PingController, InfoController])
 class MessageController:
     pass
