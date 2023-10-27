65a34ce5abfc883fe0a736f67f1bc8881d70d4eb
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-27 16:43:49 -0300

    fix: fix LanguageSelector getting wrong path of strings

--- userlixo/utils/services/language_selector.py
+++ userlixo/utils/services/language_selector.py
@@ -17,12 +17,12 @@ class LanguageSelector:
     def __init__(self):
         strings = {}
         for file in Path().glob("userlixo/strings/*.yml"):
-            string_file = file.name
-            language_code = re.match(r"userlixo/strings/(.+)\.yml$", string_file)[1]
+            string_file = "userlixo/strings/" + file.name
+            language_code = re.match(r"userlixo/strings/(.+)\.yml$", string_file).group(1)
             strings[language_code] = open_yml(string_file)
 
         self.langs = Langs(**strings, escape_html=True)
 
     def get_lang(self):
-        self.langs.code = os.environ["LANGUAGE"]
-        return self.langs.get_language(os.getenv("LANGUAGE"))
+        self.langs.code = os.getenv("LANGUAGE") or "en"
+        return self.langs.get_language(self.langs.code)
