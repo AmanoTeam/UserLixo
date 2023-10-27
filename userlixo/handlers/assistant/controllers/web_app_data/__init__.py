0cbd6c9db3ba87a2bc05cf0f4b41c369878f7d64
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-27 13:13:19 -0300

    chore(assistant): enable back WebAppDataController

--- userlixo/handlers/assistant/controllers/web_app_data/__init__.py
+++ userlixo/handlers/assistant/controllers/web_app_data/__init__.py
@@ -1,15 +1,17 @@
 from userlixo.decorators import Controller
 
 
-# from .restart_controller import RestartController
-# from .save_settings_controller import SaveSettingsController
-# from .upgrade_controller import UpgradeController
+from .restart_controller import RestartController
+from .save_settings_controller import SaveSettingsController
+from .upgrade_controller import UpgradeController
 
 
-@Controller(imports=[
-    # RestartController,
-    # SaveSettingsController,
-    # UpgradeController,
-])
+@Controller(
+    imports=[
+        RestartController,
+        SaveSettingsController,
+        UpgradeController,
+    ]
+)
 class WebAppDataController:
     pass
