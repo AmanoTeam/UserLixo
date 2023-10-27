2a6e697a2db9f4397f97897eb333e4fe017d30ba
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-26 23:11:04 -0300

    feat: implement list_commands handler and make it functional

--- userlixo/assistant/controllers/callback_query/command_controller.py
+++ userlixo/assistant/controllers/callback_query/command_controller.py
@@ -1,13 +1,21 @@
+from dataclasses import dataclass
+
 from pyrogram import filters
 
+from userlixo.assistant.handlers.callback_query.list_commands_callback_query_handler import (
+    ListCommandsCallbackQueryHandler,
+)
 from userlixo.decorators import Controller, on_callback_query
 
 
 @Controller()
+@dataclass
 class CommandController:
+    list_commands_handler: ListCommandsCallbackQueryHandler
+
     @on_callback_query(filters.regex(r"^list_commands (?P<page>\d+)"))
     async def list_commands(self, c, callback_query):
-        pass
+        await self.list_commands_handler.handle_callback_query(c, callback_query)
 
     @on_callback_query(filters.regex(r"^info_command (?P<cmd>.+) (?P<pg>\d+)"))
     async def info_command(self, c, callback_query):
---
+++ userlixo/assistant/handlers/common/commands.py
@@ -0,0 +1,26 @@
+from langs import Langs
+from pyrogram.helpers import ikb
+from pyrogram.nav import Pagination
+
+from userlixo.config import cmds
+
+
+def compose_list_commands_message(lang: Langs, page: int, append_back: bool = False):
+    item_format = "info_command {} {}"
+    page_format = "list_commands {}"
+
+    layout = Pagination(
+        [*cmds.items()],
+        item_data=lambda i, pg: item_format.format(i[0], pg),
+        item_title=lambda i, pg: i[0],
+        page_data=lambda pg: page_format.format(pg),
+    )
+
+    lines = layout.create(page, columns=2, lines=3)
+    if append_back:
+        lines.append([(lang.back, "start")])
+
+    text = lang.commands_text
+    keyboard = ikb(lines)
+
+    return text, keyboard
