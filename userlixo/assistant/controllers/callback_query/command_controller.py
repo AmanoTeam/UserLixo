0e173a00e7cf046c6bcd9a3ed32d925337ac7074
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-26 23:16:14 -0300

    feat: implement info_command handler and make it functional

--- userlixo/assistant/controllers/callback_query/command_controller.py
+++ userlixo/assistant/controllers/callback_query/command_controller.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 
 from pyrogram import filters
 
+from userlixo.assistant.handlers.callback_query.info_command_callback_query_handler import (
+    InfoCommandCallbackQueryHandler,
+)
 from userlixo.assistant.handlers.callback_query.list_commands_callback_query_handler import (
     ListCommandsCallbackQueryHandler,
 )
@@ -12,6 +15,7 @@ from userlixo.decorators import Controller, on_callback_query
 @dataclass
 class CommandController:
     list_commands_handler: ListCommandsCallbackQueryHandler
+    info_commands_handler: InfoCommandCallbackQueryHandler
 
     @on_callback_query(filters.regex(r"^list_commands (?P<page>\d+)"))
     async def list_commands(self, c, callback_query):
@@ -19,4 +23,4 @@ class CommandController:
 
     @on_callback_query(filters.regex(r"^info_command (?P<cmd>.+) (?P<pg>\d+)"))
     async def info_command(self, c, callback_query):
-        pass
+        await self.info_commands_handler.handle_callback_query(c, callback_query)
