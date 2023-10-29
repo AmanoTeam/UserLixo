b92e2c33d0b8f439df08c8b9bff83f9f3827c33c
Cezar Pauxis <pauxiscezar@gmail.com>
2023-10-29 02:59:08 -0300

    feat: add parameter sudoers_only to all on_* decorators

--- userlixo/decorators/on_callback_query.py
+++ userlixo/decorators/on_callback_query.py
@@ -1,13 +1,20 @@
 from collections.abc import Callable
 
-from pyrogram.filters import Filter
+from pyrogram.filters import Filter, user
+
+from userlixo.config import sudoers
 
 
-def on_callback_query(filters: Filter = None, group: int = 0) -> Callable:
+def on_callback_query(
+    filters: Filter = None, group: int = 0, sudoers_only: bool = True
+) -> Callable:
     def decorator(func: Callable) -> Callable:
         func.on = "callback_query"
-        func.filters = filters
         func.group = group
+
+        func.filters = filters
+        if sudoers_only:
+            func.filters = filters & user(sudoers)
 
         return func
 
--- userlixo/decorators/on_inline_query.py
+++ userlixo/decorators/on_inline_query.py
@@ -1,13 +1,18 @@
 from collections.abc import Callable
 
-from pyrogram.filters import Filter
+from pyrogram.filters import Filter, user
+
+from userlixo.config import sudoers
 
 
-def on_inline_query(filters: Filter = None, group: int = 0) -> Callable:
+def on_inline_query(filters: Filter = None, group: int = 0, sudoers_only: bool = True) -> Callable:
     def decorator(func: Callable) -> Callable:
         func.on = "inline_query"
-        func.filters = filters
         func.group = group
+
+        func.filters = filters
+        if sudoers_only:
+            func.filters = filters & user(sudoers)
 
         return func
 
