be9acce8034ce929911f74c643d635e5077bb015
Cezar Pauxis <pauxiscezar@gmail.com>
2023-11-04 20:17:24 -0300

    refactor(plugins): rename on_user_handler -> user_handler, on_bot_handler -> bot_handler

--- userlixo/decorators/__init__.py
+++ userlixo/decorators/__init__.py
@@ -1,15 +1,15 @@
+from .bot_handler import bot_handler
 from .controller import Controller
-from .on_bot_handler import on_bot_handler
 from .on_callback_query import on_callback_query
 from .on_inline_query import on_inline_query
 from .on_message import on_message
-from .on_user_handler import on_user_handler
+from .user_handler import user_handler
 
 __all__ = [
     "Controller",
     "on_callback_query",
     "on_inline_query",
     "on_message",
-    "on_bot_handler",
-    "on_user_handler",
+    "bot_handler",
+    "user_handler",
 ]
