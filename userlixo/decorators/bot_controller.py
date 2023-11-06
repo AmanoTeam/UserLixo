00985d79f6e48b7fcf6529cff9349555c5a1dd00
Cezar Pauxis <pauxiscezar@gmail.com>
2023-11-06 11:33:55 -0300

    refactor: make decorators bot_controller and user_controller usable with or without parenthesis and make them call controller() internally

--- userlixo/decorators/bot_controller.py
+++ userlixo/decorators/bot_controller.py
@@ -1,6 +1,17 @@
 from collections.abc import Callable
 
+from userlixo.decorators.controller import controller
 
-def bot_controller(func: Callable) -> Callable:
-    func.is_bot_plugin_controller = True
-    return func
+
+def bot_controller(*args, **kwargs):
+    def decorator(cls: Callable, imports: list | None = None, plugin_handler: str | None = None):
+        cls = controller(imports, plugin_handler)(cls)
+        cls.is_bot_plugin_controller = True
+        return cls
+
+    called_without_args = len(args) == 1 and callable(args[0]) and len(kwargs) == 0
+
+    if called_without_args:
+        return decorator(args[0])
+
+    return lambda cls: decorator(cls, *args, **kwargs)
--- userlixo/decorators/user_controller.py
+++ userlixo/decorators/user_controller.py
@@ -1,6 +1,17 @@
 from collections.abc import Callable
 
+from userlixo.decorators.controller import controller
 
-def user_controller(func: Callable) -> Callable:
-    func.is_user_plugin_controller = True
-    return func
+
+def user_controller(*args, **kwargs):
+    def decorator(cls: Callable, imports: list | None = None, plugin_handler: str | None = None):
+        cls = controller(imports, plugin_handler)(cls)
+        cls.is_user_plugin_controller = True
+        return cls
+
+    called_without_args = len(args) == 1 and callable(args[0]) and len(kwargs) == 0
+
+    if called_without_args:
+        return decorator(args[0])
+
+    return lambda cls: decorator(cls, *args, **kwargs)
